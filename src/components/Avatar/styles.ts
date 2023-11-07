b53c6306ce175e500bbc58b0111d5738151bfd2f
JVDourado10 <jovneiva@gmail.com>
2023-11-07 14:40:25 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const FotoPerfil = styled.img`
+  border-radius: 50%;
+  margin-bottom: 24px;
+`
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/JVDourado10.png" />
-    <Titulo fontSize={20}>João Victor Dourado Neiva Silva</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>João Victor Dourado Neiva Silva</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        JVDourado10
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
